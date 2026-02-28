a020cefb6c741bee0c062bd7e3427702f7b923d7
BOTLtd <129214895+BOTLtd@users.noreply.github.com>
2026-02-28 09:53:55 +0200

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Feed from "./pages/Feed";
+import Upload from "./pages/Upload";
+import Map from "./pages/Map";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <div>
+      <Login />
+      <Upload />
+      <Feed />
+      <Map />
+    </div>
+  );
+}
