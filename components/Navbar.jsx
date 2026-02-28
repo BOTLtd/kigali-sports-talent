01b9af74e569f17c66db0101acc6f69d72895f98
BOTLtd <129214895+BOTLtd@users.noreply.github.com>
2026-02-28 11:29:41 +0200

    Create Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <div style={{
+      position: "sticky",
+      top: 0,
+      background: "white",
+      padding: "12px",
+      textAlign: "center",
+      fontWeight: "bold",
+      borderBottom: "1px solid #eee",
+      zIndex: 10
+    }}>
+      ⚽ Kigali Sports Talent
+    </div>
+  );
+}
